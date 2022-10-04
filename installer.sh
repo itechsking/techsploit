12a3f258396e7c46613d6dbedbf2536907c906bf
techsking <87140442+itechsking@users.noreply.github.com>
2022-10-04 20:12:37 +0530

    techsploit
    # techsploit
    
    # Disclaimer:
    Usage of techsploit for attacking targets without prior mutual consent is ILLEGAL. Developers are not responsible for any damage caused by this script. techsploit is intented ONLY FOR EDUCATIONAL PURPOSES!!! STAY LEGAL!!!
    
    # How to install:-
    cd techsploit
    ./installer.sh
    # how to run
    Type anywhere in your terminal techsploit
    
    
    techsploit is inspired by https://www.patreon.com/kalilinuxtricks Big thanks to @KALILINUXTRICKSYT.

---
+++ installer.sh
@@ -0,0 +1,39 @@
+chmod +x techsploit
+cp -r techsploit /usr/local/sbin
+echo "rm -rf /usr/local/sbin/techsploit && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh " > uninstaller.sh 
+
+
+
+echo -e '\e[1;33m
+   /$$                         /$$                           /$$           /$$   /$$    
+  | $$                        | $$                          | $$          |__/  | $$    
+ /$$$$$$    /$$$$$$   /$$$$$$$| $$$$$$$   /$$$$$$$  /$$$$$$ | $$  /$$$$$$  /$$ /$$$$$$  
+|_  $$_/   /$$__  $$ /$$_____/| $$__  $$ /$$_____/ /$$__  $$| $$ /$$__  $$| $$|_  $$_/  
+  | $$    | $$$$$$$$| $$      | $$  \ $$|  $$$$$$ | $$  \ $$| $$| $$  \ $$| $$  | $$    
+  | $$ /$$| $$_____/| $$      | $$  | $$ \____  $$| $$  | $$| $$| $$  | $$| $$  | $$ /$$
+  |  $$$$/|  $$$$$$$|  $$$$$$$| $$  | $$ /$$$$$$$/| $$$$$$$/| $$|  $$$$$$/| $$  |  $$$$/
+   \___/   \_______/ \_______/|__/  |__/|_______/ | $$____/ |__/ \______/ |__/   \___/  
+                                                  | $$                                  
+                                                  | $$                                  
+                                                  |__/         
+                                
+                                Created by "techsking"
+                                https://www.youtube.com/techsking\e[1;31m
+
+
+  Usage of techsploit for attacking targets without prior mutual consent is
+  ILLEGAL. Developers are not responsible for any damage caused by this script.
+  techsploit is intented ONLY FOR EDUCATIONAL PURPOSES!!! STAY LEGAL!!!
+
+\e[1;36m
+                      
+                       (INSTALLATION COMPLETED)
+
+                                                             \e[1;32m
+
+  To execute techsploit type anywhere in your terminal "techsploit".  \e[0m 
+'
---
+++ uninstaller.sh
@@ -0,0 +1,5 @@
+rm -rf /usr/local/sbin/techsploit && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh 
